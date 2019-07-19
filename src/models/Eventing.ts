bf05c9bf09d7ba852ed09d5f05b07b27341a4f5e
christos <paschalidi@outlook.com>
2019-07-19 12:22:58 +0200

    Eventing

---
+++ src/models/Eventing.ts
@@ -0,0 +1,23 @@
+export type Callback = () => void;
+
+export class Eventing {
+  events: { [key: string]: Callback[] } = {};
+
+  on = (eventName: string, cb: Callback): void => {
+    const handlers = this.events[eventName] || [];
+    handlers.push(cb);
+    this.events[eventName] = handlers;
+  };
+
+  trigger = (eventName: string): void => {
+    const handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+
+    handlers.forEach(callback => {
+      callback();
+    });
+  };
+}
