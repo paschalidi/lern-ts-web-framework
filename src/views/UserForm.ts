d0b283f2c4525acc7dd54119d84a720bb12664f9
christos <paschalidi@outlook.com>
2019-07-21 12:46:46 +0200

    refactors USerForm with generics

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,30 +1,10 @@
-import { User } from "../models/User";
+import { View } from "./View";
+import { User, UserProps } from "../models/User";
 
-export class UserForm {
-  constructor(public parent: Element, public model: User) {
-    this.renderOnChange();
-  }
-  _addErrorForm = (): void => {
-    const errorNode = this.parent.querySelector(".error");
-    if (errorNode) {
-      errorNode.innerHTML = "";
-      errorNode.append(`Please add some text ;)`);
-    }
-  };
-
-  _clearErrorForm = (): void => {
-    const errorNode = this.parent.querySelector(".error");
-    if (errorNode) {
-      errorNode.innerHTML = "";
-    }
-  };
-  renderOnChange() {
-    this.model.on("change", () => {
-      this.render();
-    });
-  }
-
+export class UserForm extends View<User, UserProps> {
   eventsMap(): { [key: string]: () => void } {
+    const name = this.model.get("name");
+
     return {
       "click:#set-name": this.onSetName,
       "click:#set-age": this.onSetRandomAge
@@ -56,32 +36,25 @@ export class UserForm {
             <h1>user</h1>
             <div>name: ${this.model.get("name")}</div>
             <div>age: ${this.model.get("age")}</div>
-            <input id="input-with-name" type="text" value="", placeholder="insert name please"/>
+            <input id="input-with-name" type="text" value="" placeholder="${this.model.get("name")}"/>
             <button id="set-name">Change Name</button>
             <button id="set-age">Set Random Age</button>
             <div class="error"></div>
         </div>`;
   }
 
-  bindEvents(fragment: DocumentFragment): void {
-    const eventsMap = this.eventsMap();
-
-    for (let eventKey in eventsMap) {
-      const [eventName, selector] = eventKey.split(":");
-      fragment
-        .querySelectorAll(selector)
-        .forEach(element =>
-          element.addEventListener(eventName, eventsMap[eventKey])
-        );
+  _addErrorForm = (): void => {
+    const errorNode = this.parent.querySelector(".error");
+    if (errorNode) {
+      errorNode.innerHTML = "";
+      errorNode.append(`Please add some text ;)`);
     }
-  }
+  };
 
-  render(): void {
-    this.parent.innerHTML = "";
-    const templateElement = document.createElement("template");
-    templateElement.innerHTML = this.template();
-
-    this.bindEvents(templateElement.content);
-    this.parent.append(templateElement.content);
-  }
+  _clearErrorForm = (): void => {
+    const errorNode = this.parent.querySelector(".error");
+    if (errorNode) {
+      errorNode.innerHTML = "";
+    }
+  };
 }
