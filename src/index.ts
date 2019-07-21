e01e14e39a562e32e6dd2c13737249c7410dd9bd
christos <paschalidi@outlook.com>
2019-07-21 14:22:30 +0200

    updates indes.ts to handle nesting

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-import { UserForm } from "./views/UserForm";
+import { UserEdit } from "./views/UserEdit";
 import { User } from "./models/User";
 
 const user = User.buildUser({ name: "Christos", age: 27 });
 const rootElement = document.getElementById("root");
 if (rootElement) {
-  const form = new UserForm(rootElement, user);
-  form.render();
+  const userEdit = new UserEdit(rootElement, user);
+  userEdit.render();
+  console.log(userEdit)
 } else {
   throw new Error("Please add root element.");
 }
